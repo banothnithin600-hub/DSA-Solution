daf098baf23dd2f6e309504379ab5a0a22c6d668
Banoth Nithin <banothnithin600@gmail.com>
2026-07-15 19:12:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -13,26 +13,11 @@ public:
         }
         result--;
     }
-
     // Return gcd of a and b
     return result;
 }
 int gcdOfOddEvenSums(int n) {
-        if(n==1) return 1;
-        if(n==0) return 0;
-        long long evensum=0,oddsum=0,count=0;
-        long long val = n*(n+1)/2;
-        for(int i=1;i<=val;i++){
-            if(count==n){
-            if(i%2==0 ){
-            evensum+=i;
-            
-            }else{
-                oddsum+=i;
-            }
-            }
-            count--;
-        }
-    return gcd(n*n,n*(n+1));
+    //return gcd(n*n,n*(n+1));
+    return n;
     }
 };
