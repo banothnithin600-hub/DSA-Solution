b1bd6f7afcd11b14aa34cb8a312855e164eee481
Banoth Nithin <banothnithin600@gmail.com>
2026-08-03 18:09:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (68.89%) - LeetSync

---
+++ 909-stone-game/stone-game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        //Alice = nithin , Bob = rahul 
+        //Conditions:  1.If nithin wins return true or rahul wins the game return false;
+        return true;
+    }
+};
