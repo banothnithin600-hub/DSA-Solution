437f52fcbbf4ee0b4221f32ce10da9e693b28181
Banoth Nithin <banothnithin600@gmail.com>
2026-08-04 19:28:14 +0530

    Time: 1 ms (12.82%) | Memory: 8.6 MB (30.17%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> s1,s2;
+        for(auto x : s){
+            if(x == '#' && !s1.empty())
+            s1.pop();
+            else if(x != '#')
+            s1.push(x);
+        }
+
+        for(auto x : t){
+            if(x == '#' && !s2.empty())
+            s2.pop();
+            else if(x != '#')
+            s2.push(x);
+        }
+        return s1 == s2;
+    }
+};
