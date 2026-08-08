e07668045fdc3749817c9b89ee8b42c724698b4a
Banoth Nithin <banothnithin600@gmail.com>
2026-08-08 11:28:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (97.80%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int integerReplacement(int num) {
+        long long int n = num;
+    long long int  count = 0;
+       while(n>1){
+       if(n==3) n--;
+       else if(n%2==0) n/=2;
+       else if(n%4==1) n--;
+       else {n++;}
+       count++;
+       } 
+       return count;
+    }
+};
