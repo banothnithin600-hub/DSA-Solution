7bfc2a585f7c2736607e8945969bf3ac3ebd0afe
Banoth Nithin <banothnithin600@gmail.com>
2026-08-14 22:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (81.14%) - LeetSync

---
+++ 738-monotone-increasing-digits/monotone-increasing-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int monotoneIncreasingDigits(int n) {
+        string s =to_string(n);
+        int marks = s.size();
+        for(int i=s.size()-1;i>0;i--){
+            if(s[i]<s[i-1]){
+                marks=i;
+                s[i-1]--;
+                //s[i]='9';
+            }
+        }
+        for(int i=marks;i<s.size();i++){
+            s[i]='9';
+        }
+        return stoi(s);
+    }
+};
