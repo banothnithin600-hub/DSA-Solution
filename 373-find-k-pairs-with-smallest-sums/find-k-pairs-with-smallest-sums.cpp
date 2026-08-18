ad8744280d5fb3a8e5be946902b0b937e002c8b8
Banoth Nithin <banothnithin600@gmail.com>
2026-08-18 19:13:20 +0530

    Time: 87 ms (48.58%) | Memory: 165.2 MB (44.87%) - LeetSync

---
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+
+        priority_queue<
+            pair<int, pair<int, int>>,
+            vector<pair<int, pair<int, int>>>,
+            greater<pair<int, pair<int, int>>>
+        > pq;
+
+        int n = nums1.size();
+        int m = nums2.size();
+
+        int sum = nums1[0] + nums2[0];
+
+        set<pair<int, int>> visited;
+
+        pq.push({sum, {0, 0}});
+        visited.insert({0, 0});
+
+        vector<vector<int>> ans;
+
+        while (k-- && !pq.empty()) {
+
+            auto temp = pq.top();
+            pq.pop();
+
+            int i = temp.second.first;
+            int j = temp.second.second;
+
+            ans.push_back({nums1[i], nums2[j]});
+
+            // (i, j + 1)
+            if (j + 1 < m &&
+                visited.find({i, j + 1}) == visited.end()) {
+
+                pq.push({
+                    nums1[i] + nums2[j + 1],
+                    {i, j + 1}
+                });
+
+                visited.insert({i, j + 1});
+            }
+
+            // (i + 1, j)
+            if (i + 1 < n &&
+                visited.find({i + 1, j}) == visited.end()) {
+
+                pq.push({
+                    nums1[i + 1] + nums2[j],
+                    {i + 1, j}
+                });
+
+                visited.insert({i + 1, j});
+            }
+        }
+
+        return ans;
+    }
+};
