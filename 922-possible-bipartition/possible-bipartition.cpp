d491e8006fe61ec2c9f802e0b948cba323a8cbc8
Banoth Nithin <banothnithin600@gmail.com>
2026-08-27 12:30:38 +0530

    Time: 27 ms (88.14%) | Memory: 68.7 MB (93.71%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.cpp
+++ 922-possible-bipartition/possible-bipartition.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    bool dfs(int src,bool cur_color,vector<bool>&vis,vector<bool>&color,vector<vector<int>>&adj){
+        int u = src;
+        vis[src]=true;
+        color[src]=cur_color;
+        for(int v:adj[src]){
+           if(!vis[v]){
+            if(!dfs(v,!cur_color,vis,color,adj)){
+                return false;
+            }
+            }else{
+                if(color[v]==color[u]){
+                    return false;
+                }
+            }
+           }
+        
+        return true;
+    }
     bool bfs(int src,vector<bool>&vis,vector<bool>&color,vector<vector<int>>&adj){
         queue<int>q;
         vis[src]=true;
@@ -34,7 +52,7 @@ public:
         vector<bool>color(n+1,false);
         for(int i=1;i<=n;i++){
             if(!vis[i]){
-                if(!bfs(i,vis,color,adj)){
+                if(!dfs(i,true,vis,color,adj)){
                     return false;
                 }
             }
