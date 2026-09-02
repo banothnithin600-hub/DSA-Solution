7309198467992b5cb589a06d525455f8e7cac691
Banoth Nithin <banothnithin600@gmail.com>
2026-09-02 11:37:41 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (36.43%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int oddcount =0;
-        for(int x:nums1){
-            if(x%2!=0) oddcount++;
-        }
+        // int oddcount =0;
+        // for(int x:nums1){
+        //     if(x%2!=0) oddcount++;
+        // }
         return true;
     }
 };
