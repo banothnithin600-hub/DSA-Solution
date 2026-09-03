e78b950d9fc7e2171cbf73244eff27871ae0a0ed
Banoth Nithin <banothnithin600@gmail.com>
2026-09-03 14:59:20 +0530

    Time: 328 ms (29.80%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,7 +1,9 @@
-select (select distinct salary from Employee
-order by salary Desc
-limit 1 OFFSET 1) as SecondHighestSalary; #This code not run for duplic.. value
+#select (select distinct salary from Employee
+#order by salary Desc
+#limit 1 OFFSET 1) as SecondHighestSalary; #This code not run for duplic.. value
 
 # We are follow two step to find second max :- subquery
  #select salary from Employee
- 
+ SELECT MAX(salary) AS SecondHighestSalary
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
