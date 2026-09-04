3f089b44e8c9ec00fa0e3744d857ef759159c127
Banoth Nithin <banothnithin600@gmail.com>
2026-09-04 19:57:14 +0530

    Time: 17 ms (69.73%) | Memory: 60.1 MB (17.35%) - LeetSync

---
+++ 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        vector<int>freq(26,0);
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']++;
+        }
+        for(int i=1;i<26;i++){
+            if(freq[i]!=0){
+                return (26-i);
+            }
+        }
+        return 0;
+    }
+};
